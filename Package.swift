7d6d8af424cd2eb67bf003c9d91f3ea39a3f56d1
Eli1234 <elia@paygilant.com>
2026-01-07 15:19:48 +0200

    4.1.1

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PaygilantSDK",
-            url: "https://paygilant-artifacts-eu-central-1.s3.eu-west-1.amazonaws.com/releases/4.0.15/PaygilantSDK.xcframework.zip",
-            checksum: "393634a290bbbee95133e34b0d12d9856fd3ea6a767b0d3d2479bf24abef6157"
+            url: "https://paygilant-artifacts-eu-central-1.s3.eu-west-1.amazonaws.com/releases/4.1.1/PaygilantSDK.xcframework.zip",
+            checksum: "d39417305ed786910da750607f2dd68dacb62c9c16a97881f30336c37472d9c9"
         )
     ]
 )
