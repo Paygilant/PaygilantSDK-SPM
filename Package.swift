26dc9115bbba75b1c0658aa1a7174b6db8a14cfe
Eli1234 <elia@paygilant.com>
2026-01-26 10:37:06 +0200

    Update PaygilantSDK to version 4.1.2
    Bumped the PaygilantSDK binary target to version 4.1.2 and updated the checksum in Package.swift.

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PaygilantSDK",
-            url: "https://paygilant-artifacts-eu-central-1.s3.eu-west-1.amazonaws.com/releases/4.1.1/PaygilantSDK.xcframework.zip",
-            checksum: "d39417305ed786910da750607f2dd68dacb62c9c16a97881f30336c37472d9c9"
+            url: "https://paygilant-artifacts-eu-central-1.s3.eu-west-1.amazonaws.com/releases/4.1.2/PaygilantSDK.xcframework.zip",
+            checksum: "95196e3d140e0b1ffc5736151422f7385625f39cc1b891f26144b2e48570cac1"
         )
     ]
 )
