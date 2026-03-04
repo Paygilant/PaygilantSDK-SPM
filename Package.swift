995c69065d811698389b58e4329a4b6e9827d740
Eli1234 <elia@paygilant.com>
2026-03-04 15:24:31 +0200

    Bump PaygilantSDK to 4.1.3
    Update the PaygilantSDK binary target to release 4.1.3 by changing the package URL and checksum in Package.swift. The diff also includes an Xcode user interface state (.xcuserstate) change (IDE metadata).

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PaygilantSDK",
-            url: "https://paygilant-artifacts-eu-central-1.s3.eu-west-1.amazonaws.com/releases/4.1.2/PaygilantSDK.xcframework.zip",
-            checksum: "95196e3d140e0b1ffc5736151422f7385625f39cc1b891f26144b2e48570cac1"
+            url: "https://paygilant-artifacts-eu-central-1.s3.eu-west-1.amazonaws.com/releases/4.1.3/PaygilantSDK.xcframework.zip",
+            checksum: "141571656ca9fca2068922adb16f83972fbf9f1d0d72ed939445c696e1dd3bd9"
         )
     ]
 )
