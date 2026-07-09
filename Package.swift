f338b81fda94d285d78d4ce5f34787141c59a64b
Eli1234 <elia@paygilant.com>
2026-07-09 09:58:13 +0300

    Bump PaygilantSDK binary to 4.1.15
    Update the Swift package binary target URL and checksum in Package.swift to point to the 4.1.15 PaygilantSDK xcframework release.

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PaygilantSDK",
-            url: "https://paygilant-artifacts-eu-central-1.s3.eu-west-1.amazonaws.com/releases/4.1.14/PaygilantSDK.xcframework.zip",
-                        checksum: "cbf02c2300cf677baafee708fc7c9eced01c51012b6562642c9378f36613d95d"
+            url: "https://paygilant-artifacts-eu-central-1.s3.eu-west-1.amazonaws.com/releases/4.1.15/PaygilantSDK.xcframework.zip",
+                        checksum: "952073bf705ae937ed73ed6abe505d5e852edd4be0ccf4497cc2134ad43c3d3e"
         )
     ]
 )
