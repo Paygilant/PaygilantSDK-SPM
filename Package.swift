9e735fac329746eafbc9b9006ff6ff094c2679ec
Eli1234 <elia@paygilant.com>
2026-08-26 10:46:12 +0300

    Bump PaygilantSDK binary to 4.2.1-beta

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PaygilantSDK",
-            url: "https://paygilant-artifacts-eu-central-1.s3.eu-west-1.amazonaws.com/releases/4.1.15/PaygilantSDK.xcframework.zip",
-                        checksum: "952073bf705ae937ed73ed6abe505d5e852edd4be0ccf4497cc2134ad43c3d3e"
+            url: "https://paygilant-artifacts-eu-central-1.s3.eu-west-1.amazonaws.com/releases/4.2.1-beta/PaygilantSDK.xcframework.zip",
+            checksum: "e1d295970130143aee634d939f56be0cf05ef5eb661001300c14889843236218"
         )
     ]
 )
