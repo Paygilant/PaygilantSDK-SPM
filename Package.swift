737db727fcead79231f666fbd44ea210ca3e2165
Eli1234 <elia@paygilant.com>
2026-09-17 11:13:10 +0300

    Revert "Raise minimum iOS platform to 15.0"
    This reverts commit df1c3ab8c3d8bcf7ebccde26f078e90248f7eec1.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "PaygilantSDK",
-    platforms: [ .iOS(.v15) ],
+    platforms: [ .iOS(.v12) ],
     products: [
         .library(name: "PaygilantSDK", targets: ["PaygilantSDK"])
     ],
